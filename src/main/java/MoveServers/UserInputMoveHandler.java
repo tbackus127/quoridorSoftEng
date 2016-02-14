ad22d04f04a5f55df192a97e26e49be1eb1fa770
Tyler Fiacco <Tyler_Fiacco@yahoo.com>
2016-02-13 20:50:05 -0500

    The main of the user move handler is almost able to handle all the arguments

--- src/main/java/MoveServers/UserInputMoveHandler.java
+++ src/main/java/MoveServers/UserInputMoveHandler.java
@@ -11,14 +11,37 @@ public class UserInputMoveHandler implements MoveServer{
     
     // Methods
     
-    /* This is the constructor to the move server. It can be passed a string which is 
+    /* This is the constructor to the move server. It can be passed a string 
+     * which is 
      */
     public UserInputMoveHandler(int initPort){
 	this.port = initPort;
     }
     
+    /* This is here to allow for the server to be ran without some outside 
+     * class. It also allows for you to input the port you would like and also
+     * allows for you to override the default name given to the server.
+     */
     public static void main(String[] args){
-	
+	int portValue;
+	int ixargs = 0;
+	// While loop  to run through all of trhe command line arguments
+	while(ixargs > args.length()){
+	    if(args[ixargs].equals("--port")){
+		ixargs++;
+		try{
+		    portValue = Integer.parseInt(args[ixargs]);
+		}catch{
+		    System.out.println("After the port argument you entered" +
+		                       " a non-numerical character");
+		    System.exit(0);
+		}
+	    }
+	    else if(args[ixargs].equals("--name")){
+		
+	    }
+	    ixargs++;
+	}
     }
     
     
