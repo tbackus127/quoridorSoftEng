1e1bcd9963b3a5e1f558f3f2052f8d5129abae4a
Frances Anastasia <anastafj197@potsdam.edu>
2016-03-20 20:42:41 -0400

    mergeing

--- teamMurka/src/main/java/AI/AIServer.java
+++ teamMurka/src/main/java/AI/AIServer.java
@@ -8,41 +8,41 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 import java.util.Scanner;
+import java.util.*;
 
-public interface MoveServer {
-  public Move requestMove() 
-  
-}
-
-public class AIServer extends ManualInputServer {
-  // @override  
-  public Move sendMove(PrintStream cout) {
+public class FranksAIServer extends ManualInputServer {
+    // @override  
+    public void sendMove(PrintStream cout) {
+        Random rand = new Random();
+        int choice = rand.nextInt(3);
+        int moves = rand.nextInt(9);
+        int walls = rand.nextInt(8);
+        if (choice == 0) {
+            if (isLegalMove == true) {
+            cout.print(moveWrapper("m " + moves + " " + walls));
+            }
+        }
+        if (choice == 1) {
+            if (isLegalWall == true) {
+                
+            }
+            
+        }
+        if (choice == 2) {
+            
+        }
+        int m = 0;
+        int h = 1; 
+        int v = 2; 
+        // checks all possible moves
+        // wall or advance 
+        // make one 
+        
+        cout.print(moveWrapper("v h m"));
+    }
       
-    // the while loop checks for in valid input and breaks if so 
-    // then if the argument is in the correct format executes move
-    
-    while (cout.hasNextLine()) {
-      moveComand = cout.nextLine();
-      String[] splitter = moveComand.split(" ");
-      if (splitter.length != 2) {
-        cout.println(moveComand + " - invalid format ");
-        break;
-      } else {
-        if (!(splitter[0].equals("m" || "v" || "h"))) {
-          cout.println("You must enter a command for a move(m) or a wall(v or h)");
-          break;
-        } else {
-          columnSel = Interger.parseInt(splitter[1]);
-          if (columnSel < 0 || columnSel > 8) {
-            cout.println("Your selection is out of bounds choose between 0 & 8");
-            break;
-          }
-          rowSel = Interger.parseInt(splitter[2]);
-          if (rowSel < 0 || rowSel > 8) {
-            cout.println("Your selection is out of bounds choose between 0 & 8");
-          } else {
-            system.out.println("Works");
-            if (splitter[0].equals("m") {
+   
+           // if (splitter[0].equals("m") {
               // turn off switch at current position 
               // move pawn to the disired coordinate 
               // update board
@@ -51,22 +51,17 @@ public class AIServer extends ManualInputServer {
               // if the player 3 gets to column 9 player one wins game
               // if the player 4 gets to column 0 player one wins game
 
-            }
-            if (splitter[0].equals("v") {
+           // }
+            //if (splitter[0].equals("v") {
               // if there still remains a path across after wall is placed
               // place verticle wall at chosen coordinate 
               // update board
-            }
-            if (splitter[0].equals("h") {
+          //  }
+          //  if (splitter[0].equals("h") {
               // if there still remains a path across after wall is placed
               // place horizontal wall at chosen coordinate
               // update board 
-            }
-          } 
-        }
-      }
-    }
-  }
+   
   public void thinker(code) {
     // Should we always make the same first move 
     // (A wall one space to the right and forward from the opponent)
