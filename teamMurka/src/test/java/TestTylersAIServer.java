5e61190c12ae6199dcb774c40af329072903a924
Tyler Fiacco <Tyler_Fiacco@yahoo.com>
2016-04-27 11:52:16 -0400

    Began working on Tyler's AI, also started the test file for it

---
+++ teamMurka/src/test/java/TestTylersAIServer.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.net.Socket;
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+import java.io.PrintStream;
+
+import com.tmquoridor.Server.*;
+import com.tmquoridor.Board.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.testng.PowerMockTestCase;
+import org.powermock.reflect.Whitebox;
+
+public class TestTylersAIServer{
+    
+    @Test
+    public void testConstructor() throws Exception {        
+        // String serverName = "America";
+        int port = 1478;
+        TylersAIServer user = new TylersAIServer(port, "mur:America", 250);
+
+        assertNotNull("Nothing was constructed", user);
+    }
+    
+}
