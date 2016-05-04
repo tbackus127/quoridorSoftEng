04b8055f0e5c826276e4256df9e501b57d061b89
Frances Anastasia <anastafj197@potsdam.edu>
2016-05-04 09:46:58 -0400

    added 2 and 4 player scripts

--- teamMurka/4-script.sh
+++ teamMurka/4-script.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+
+./server.sh 9996 | sed "s/^/PLAYER 1: /" &
+./server.sh 9997 | sed "s/^/PLAYER 2: /" &
+./server.sh 9998 | sed "s/^/PLAYER 3: /" &
+./server.sh 9999 | sed "s/^/PLAYER 4: /" &
+
+# give the servers time to start up
+sleep 1
+
+./client.sh 9996 9997 9998 9999
--- teamMurka/client-script.sh
+++ teamMurka/client-script.sh
@@ -1,1 +1,10 @@
 #!/bin/sh
+
+if [[ $# == 2 || $# == 4 ]]; then
+    java -cp build/libs/Quoridor-teamMurka-1.0.jar com.tmquoridor Client.GameClient $@
+else
+    echo "Usage:"
+    echo "  ./client <host1>:<port1> <host2>:<port2> \\"
+    echo "           [<host3:<port3> <host4>:<port4>]"
+    echo "Host names are optional (default: localhost)"
+fi
